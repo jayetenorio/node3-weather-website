a8961bab649b029f7287d8de2ae19be327331eae
Jaye <jaye.tenorio@veridata.com.ph>
2019-12-09 17:13:53 +0800

    Init commit

---
+++ src/utils/forcast.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+const forecast = (latitude, longtitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/06ef4f86977c6aba892b64947d19e4c3/' +
+    latitude +
+    ',' +
+    longtitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location. try another search.', undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ' It is currently ' +
+          body.currently.temperature +
+          ' degrees out. There is a ' +
+          body.currently.precipProbability +
+          '% chance of rain.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
